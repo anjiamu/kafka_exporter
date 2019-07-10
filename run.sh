94577a5b44121b66f4f959834489c31602815973
ezhnwng <zhengjia.wang@ericsson.com>
2019-07-10 15:30:20 +0800

    DOckerfile3

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-echo ${cac} > /etc/exmple2/ca-cert \
-    && echo ${keystorePemc} > /etc/exmple2/kafka.client.keystore.pem \
-    && echo ${keystoreKeyc} > /etc/exmple2/kafka.client.keystore.key \
+echo -e ${cac} > /etc/exmple2/ca-cert \
+    && echo -e ${keystorePemc} > /etc/exmple2/kafka.client.keystore.pem \
+    && echo -e ${keystoreKeyc} > /etc/exmple2/kafka.client.keystore.key \
     && /bin/kafka_exporter --kafka.server=${kafka_server_addr} --tls.enabled --no-sasl.handshake --tls.insecure-skip-tls-verify --tls.ca-file=/etc/exmple2/ca-cert --tls.cert-file=/etc/exmple2/kafka.client.keystore.pem --tls.key-file=/etc/exmple2/kafka.client.keystore.key
 #nohup /bin/kafka_exporter --kafka.server=${kafka_server_addr} --tls.enabled --no-sasl.handshake --tls.insecure-skip-tls-verify --tls.ca-file=/etc/exmple2/ca-cert --tls.cert-file=/etc/exmple2/kafka.client.keystore.pem --tls.key-file=/etc/exmple2/kafka.client.keystore.key > /tmp.log &
 
